b1203897415fb0fdaa6c0f260f8c5bf4f729b42d
Rory Doak <rory.doak@gmail.com>
2024-01-23 19:43:03 +0000

    initial route and controller setup

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
 
+const userRoutes = require("./routes/users-routes");
+const battlesRoutes = require("./routes/battles-routes");
+const rankingsRoutes = require("./routes/rankings-routes");
+const armiesRoutes = require("./routes/armies-routes");
+
 app.use(express.json());
 app.use(cors());
+
+app.use("/users", userRoutes);
+app.use("/battles", battlesRoutes);
+app.use("/rankings", rankingsRoutes);
+app.use("/armies", armiesRoutes);
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);

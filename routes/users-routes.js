744245f5bdb6765d9ef04fccd5dd001545a85f52
Rory Doak <rory.doak@gmail.com>
2024-01-24 12:13:57 +0000

    BE1.5 add route for login and generating jwt token

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -37,4 +37,36 @@ router.route("/register").post(async (req, res) => {
   }
 });
 
+router.route("/login").post(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email | !password) {
+    return res.status(400).send("Please send required information");
+  }
+
+  try {
+    const user = await knex("users").where({ email: email }).first();
+
+    if (!user) {
+      return res.status(400).send("User has not been found");
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).send("Invalid Password");
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY,
+      { expiresIn: "24h" }
+    );
+    res.send(token);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Unable to find user");
+  }
+});
+
 module.exports = router;

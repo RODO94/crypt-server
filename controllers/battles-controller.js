32dc090c02508ba4a8774a6023cf681d3f66bd8c
Rory Doak <rory.doak@gmail.com>
2024-01-25 22:04:36 +0000

    progress BE2.2 and add scenario to battle schema

--- controllers/battles-controller.js
+++ controllers/battles-controller.js
@@ -17,4 +17,55 @@ const multiplayerKnexInsert = async (playerArray, teamID) => {
   return teamArray;
 };
 
-module.exports = { multiplayerKnexInsert };
+const singleToMultiCombatantUpdate = async (
+  playerArray,
+  prevPlayerID,
+  teamID
+) => {
+  for (i = o; i < playerArray.length; i++) {
+    if (playerArray[i].id === prevPlayerID) {
+      await knex("combatant").where(
+        { id: prevPlayerID }.update({ team_id: teamID })
+      );
+    } else {
+      await knex("combatants").insert({
+        id: crypto.randomUUID(),
+        army_id: playerArray[i].army_id,
+        team_id: teamID,
+      });
+    }
+  }
+
+  return await knex("combtant").where({ team_id: teamID });
+};
+
+const deleteCombatantTeam = async (teamID) => {
+  await knex("combatants").where({ team_id: teamID }).del();
+  return;
+};
+
+const addCombatant = async (army_id, combatantID) => {
+  await knex("combatant").insert({ id: combatantID, army_id: army_id });
+
+  return await knex("combatant").where({ id: combatantID }).first();
+};
+
+const assignNewCombatant = async (battleID, combatantID, player) => {
+  if (player === 1) {
+    await knex("battle")
+      .where({ id: battleID })
+      .update({ player_1_id: combatantID });
+  } else if (player === 2) {
+    await knex("battle")
+      .where({ id: battleID })
+      .update({ player_2_id: combatantID });
+  }
+};
+
+module.exports = {
+  multiplayerKnexInsert,
+  singleToMultiCombatantUpdate,
+  deleteCombatantTeam,
+  addCombatant,
+  assignNewCombatant,
+};
--- routes/battles-routes.js
+++ routes/battles-routes.js
@@ -6,7 +6,13 @@ const dayjs = require("dayjs");
 
 require("dotenv").config();
 
-const { multiplayerKnexInsert } = require("../controllers/battles-controller");
+const {
+  multiplayerKnexInsert,
+  singleToMultiCombatantUpdate,
+  deleteCombatantTeam,
+  addCombatant,
+  assignNewCombatant,
+} = require("../controllers/battles-controller");
 
 router.route("/create").post(async (req, res) => {
   let { points_size, battle_type, player_type, player_1, player_2, date } =
@@ -118,28 +124,242 @@ router.route("/create").post(async (req, res) => {
 router.route("/:id/edit/pointsize").patch(async (req, res) => {
   const battleID = req.params.id;
   const { points_size } = req.body;
-
+  const oldPointsSize = await knex("battles")
+    .where({ id: battleID })
+    .first()
+    .select("points_size");
   try {
     await knex("battles")
       .where({ id: battleID })
       .update({ points_size: points_size });
-  } catch (error) {}
+    res
+      .status(200)
+      .send(
+        `Points Size has been updated from ${oldPointsSize.points_size} to ${points_size}`
+      );
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Issue with altering points size in the database");
+  }
 });
 router.route("/:id/edit/scenario").patch(async (req, res) => {
   const battleID = req.params.id;
   const { scenario } = req.body;
+
+  const oldScenario = await knex("battles")
+    .where({ id: battleID })
+    .first()
+    .select("scenario");
+  try {
+    await knex("battles")
+      .where({ id: battleID })
+      .update({ scenario: scenario });
+    res
+      .status(200)
+      .send(
+        `Scenario has been updated from ${oldScenario.scenario} to ${scenario}`
+      );
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Issue with altering scenario in the database");
+  }
 });
 router.route("/:id/edit/date").patch(async (req, res) => {
   const battleID = req.params.id;
   const { date } = req.body;
+
+  const oldDate = await knex("battles")
+    .where({ id: battleID })
+    .first()
+    .select("date");
+  try {
+    await knex("battles")
+      .where({ id: battleID })
+      .update({ date: dayjs(date).format("YYYY-MM-DD HH:mm:ss") });
+    res
+      .status(200)
+      .send(
+        `Date has been updated from ${oldDate.date} to ${dayjs(date).format(
+          "YYYY-MM-DD HH:mm:ss"
+        )}`
+      );
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Issue with altering date in the database");
+  }
 });
 router.route("/:id/edit/gametype").patch(async (req, res) => {
   const battleID = req.params.id;
   const { battle_type, player_type } = req.body;
+
+  if (!battle_type && player_type) {
+    const oldPlayerType = await knex("battles")
+      .where({ id: battleID })
+      .first()
+      .select("player_type");
+    try {
+      await knex("battles")
+        .where({ id: battleID })
+        .update({ player_type: player_type });
+      res
+        .status(200)
+        .send(
+          `Player Type has been updated from ${oldPlayerType.player_type} to ${player_type}`
+        );
+    } catch (error) {
+      console.error(error);
+      res.status(400).send("Issue with altering Player Type in the database");
+    }
+  } else if (battle_type && !player_type) {
+    const oldBattleType = await knex("battles")
+      .where({ id: battleID })
+      .first()
+      .select("battle_type");
+    try {
+      await knex("battles")
+        .where({ id: battleID })
+        .update({ battle_type: battle_type });
+      res
+        .status(200)
+        .send(
+          `Battle Type has been updated from ${oldBattleType.battle_type} to ${battle_type}`
+        );
+    } catch (error) {
+      console.error(error);
+      res.status(400).send("Issue with altering Battle Type in the database");
+    }
+  } else if (battle_type && player_type) {
+    const oldBattleType = await knex("battles")
+      .where({ id: battleID })
+      .first()
+      .select("battle_type", "player_type");
+    try {
+      await knex("battles")
+        .where({ id: battleID })
+        .update({ player_type: player_type, battle_type: battle_type });
+      res
+        .status(200)
+        .send(
+          `Player Type has been updated from ${oldBattleType.player_type} to ${player_type} and Battle Type has been changed from ${oldBattleType.battle_type} to ${battle_type}`
+        );
+    } catch (error) {
+      console.error(error);
+      res.status(400).send("Issue with altering Player Type in the database");
+    }
+  } else {
+    res
+      .status(400)
+      .send(
+        `Required details have not been submitted, please recheck your submission`
+      );
+  }
 });
 router.route("/:id/edit/combatants").patch(async (req, res) => {
   const battleID = req.params.id;
   const { player_1, player_2 } = req.body;
+
+  const battleToChange = await knex("battles").where({ id: battleID }).first();
+
+  if (!battleToChange) {
+    return res
+      .status(400)
+      .send("Unable to locate the battle you are looking for");
+  }
+
+  const prevPlayerOneID = battleToChange.player_1_id;
+  const prevPlayerTwoID = battleToChange.player_2_id;
+  // The case where the game is single but they have added another player
+  // making it multiplayer.
+  //   Player ID's would need to switch to Team IDs, update the combatants
+
+  if (
+    battleToChange.player_type === "single" &&
+    (player_1.length > 1) | (player_2.length > 1)
+  ) {
+    try {
+      const teamOneID = crypto.randomUUID();
+      const teamTwoID = crypto.randomUUID();
+
+      const teamOneArray = await singleToMultiCombatantUpdate(
+        player_1,
+        prevPlayerOneID,
+        teamOneID
+      );
+      const teamTwoArray = await singleToMultiCombatantUpdate(
+        player_2,
+        prevPlayerTwoID,
+        teamTwoID
+      );
+
+      await knex("battles")
+        .where({ id: battleID })
+        .update({ player_type: "multi" });
+
+      res.status(200).send({
+        message: "Teams successfully Update",
+        teamOne: teamOneArray,
+        teamTwo: teamTwoArray,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(400).send("Unable to update teams");
+    }
+  } else if (
+    battleToChange.player_type === "single" &&
+    player_1.length === 1 &&
+    player_2.length === 1
+  ) {
+    // Check what has changed
+    // Alter the army_id of the combatant
+    try {
+      if (player_1.id !== prevPlayerOneID) {
+        await knex("combatants")
+          .where({ id: prevPlayerOneID })
+          .update({ army_id: player_1.army_id });
+      }
+      if (player_2.id !== prevPlayerTwoID) {
+        await knex("combatants")
+          .where({ id: prevPlayerTwoID })
+          .update({ army_id: player_2.army_id });
+      }
+
+      res.status(200).send({
+        message: "Successfully updated the combatants",
+        player_1,
+        player_2,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(400).send("Unable to update the combatants");
+    }
+  } else if (
+    battleToChange.player_type === "multi" &&
+    player_1.length === 1 &&
+    player_2.length === 1
+  ) {
+    const playerOneID = crypto.randomUUID();
+    const playerTwoID = crypto.randomUUID();
+
+    try {
+      await deleteCombatantTeam(battleToChange.player_1_id);
+      await deleteCombatantTeam(battleToChange.player_2_id);
+
+      const playerOneObj = await addCombatant(player_1.army_id, playerOneID);
+      const playerTwoObj = await addCombatant(player_2.army_id, playerTwoID);
+
+      await assignNewCombatant(battleID, playerOneID, 1);
+      await assignNewCombatant(battleID, playerTwoID, 2);
+
+      res.status(200).send({
+        message: "Combatants have been successfully update",
+        player_1: playerOneObj,
+        player_2: playerTwoObj,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(400).send("Unable to update the battle");
+    }
+  }
 });
 
 module.exports = router;

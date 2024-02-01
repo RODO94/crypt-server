6f6e70fa4583b6c2ef41147b7d6a380cd5f4f518
Rory Doak <rory.doak@gmail.com>
2024-02-01 14:15:20 +0000

    BE4.4 route to retrieve top 5 rankings

--- routes/rankings-routes.js
+++ routes/rankings-routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const knex = require("knex");
-const { fetchAllRankings } = require("../controllers/rankings-controllers");
+const {
+  fetchAllRankings,
+  fetchTopFiveRanking,
+} = require("../controllers/rankings-controllers");
 const router = express.Router();
 
 router.route("/all").get(fetchAllRankings);
+router.route("/top5").get(fetchTopFiveRanking);
 
 module.exports = router;

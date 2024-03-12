a5e2142db9b09addfa7da35a1456d71dd0e9d2b9
Rory Doak <rory.doak@gmail.com>
2024-03-12 09:58:40 +0000

    alter ranking fetches to reduce knex operation with rank_view

--- controllers/rankings-controllers.js
+++ controllers/rankings-controllers.js
@@ -2,49 +2,19 @@ const knex = require("knex")(require("../knexfile"));
 
 const fetchAllRankings = async (req, res) => {
   try {
-    const fortySubquery = knex("rank")
-      .join("armies", "rank.army_id", "=", "armies.id")
-      .join("users", "armies.user_id", "=", "users.id")
-      .select(
-        "army_id",
-        "date",
-        "ranking",
-        "armies.name",
-        "users.known_as",
-        "prev_ranking"
-      )
-      .rowNumber("rn", { column: "date", order: "desc" }, "army_id")
-      .where({ "armies.type": "40k" })
-      .as("fortyranks");
+    const rankArray = await knex("rank_view")
+      .join("armies", "armies.id", "=", "army_id")
+      .where("rn", "=", 1);
 
-    const fortyQuery = knex(fortySubquery)
-      .select("name", "known_as", "ranking", "rn", "prev_ranking", "army_id")
-      .where("rn", 1)
-      .orderBy("ranking", "desc");
+    const fantasyArray = rankArray.filter(
+      (army) => army.type.toLowerCase() === "fantasy"
+    );
 
-    const fantasySubquery = knex("rank")
-      .join("armies", "rank.army_id", "=", "armies.id")
-      .join("users", "armies.user_id", "=", "users.id")
-      .select(
-        "army_id",
-        "date",
-        "ranking",
-        "armies.name",
-        "users.known_as",
-        "prev_ranking"
-      )
-      .rowNumber("fn", { column: "date", order: "desc" }, "army_id")
-      .where({ "armies.type": "fantasy" })
-      .as("fantasyranks");
+    const fortykArray = rankArray.filter(
+      (army) => army.type.toLowerCase() === "40k"
+    );
 
-    const fantasyQuery = knex(fantasySubquery)
-      .select("name", "known_as", "ranking", "fn", "prev_ranking", "army_id")
-      .where("fn", 1)
-      .orderBy("ranking", "desc");
-
-    res
-      .status(200)
-      .send({ fortyK: await fortyQuery, fantasy: await fantasyQuery });
+    res.status(200).send({ fortyK: fortykArray, fantasy: fantasyArray });
   } catch (error) {
     console.error(error);
     res.status(400).send("Unable to retrieve rankings");
@@ -53,51 +23,33 @@ const fetchAllRankings = async (req, res) => {
 
 const fetchTopFiveRanking = async (req, res) => {
   try {
-    const fortySubquery = knex("rank")
-      .join("armies", "rank.army_id", "=", "armies.id")
-      .join("users", "armies.user_id", "=", "users.id")
-      .select(
-        "army_id",
-        "date",
-        "ranking",
-        "armies.name",
-        "users.known_as",
-        "prev_ranking"
-      )
-      .rowNumber("rn", { column: "date", order: "desc" }, "army_id")
-      .where({ "armies.type": "40k" })
-      .as("fortyranks");
+    const rankArray = await knex("rank_view")
+      .join("armies", "armies.id", "=", "army_id")
+      .where("rn", "=", 1);
 
-    const fortyQuery = knex(fortySubquery)
-      .select("name", "known_as", "ranking", "rn", "prev_ranking", "army_id")
-      .where("rn", 1)
-      .orderBy("ranking", "desc")
-      .limit(5);
+    const fantasyArray = rankArray
+      .filter((army) => army.type.toLowerCase() === "fantasy")
+      .map((army, index) => {
+        if (index < 5) {
+          return army;
+        } else {
+          return " ";
+        }
+      })
+      .filter((army) => army !== " ");
 
-    const fantasySubquery = knex("rank")
-      .join("armies", "rank.army_id", "=", "armies.id")
-      .join("users", "armies.user_id", "=", "users.id")
-      .select(
-        "army_id",
-        "date",
-        "ranking",
-        "armies.name",
-        "users.known_as",
-        "prev_ranking"
-      )
-      .rowNumber("fn", { column: "date", order: "desc" }, "army_id")
-      .where({ "armies.type": "fantasy" })
-      .as("fantasyranks");
+    const fortykArray = rankArray
+      .filter((army) => army.type.toLowerCase() === "40k")
+      .map((army, index) => {
+        if (index < 5) {
+          return army;
+        } else {
+          return " ";
+        }
+      })
+      .filter((army) => army !== " ");
 
-    const fantasyQuery = knex(fantasySubquery)
-      .select("name", "known_as", "ranking", "fn", "prev_ranking", "army_id")
-      .where("fn", 1)
-      .orderBy("ranking", "desc")
-      .limit(5);
-
-    res
-      .status(200)
-      .send({ fortyK: await fortyQuery, fantasy: await fantasyQuery });
+    res.status(200).send({ fortyk: fortykArray, fantasy: fantasyArray });
   } catch (error) {
     console.error(error);
     res.status(400).send("Unable to retrieve rankings");

2d796ddea1cc0f8ccd17712978b4d5114adca657
Rory Doak <rory.doak@gmail.com>
2024-03-12 10:14:37 +0000

    removing first rank_view migration

--- migrations/20240308150559_battle-views.js
+++ migrations/20240308150559_battle-views.js
@@ -2,22 +2,10 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-  return knex.schema.createView("rank_view", function (view) {
-    view.columns(["army_id", "date", "ranking", "rn"]);
-    view.as(
-      knex("rank")
-        .select("army_id", "date", "ranking")
-        .rowNumber("rn", { column: "date", order: "desc" }, "army_id")
-        .as("ranks")
-    );
-  });
-};
+exports.up = function (knex) {};
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
-  return knex.schema.dropView("rank_view");
-};
+exports.down = function (knex) {};
